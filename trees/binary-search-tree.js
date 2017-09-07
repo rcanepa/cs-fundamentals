fefaa8cbe9348d90b9ee325bc511710393e0748d
Renzo Canepa <rcanepag@gmail.com>
2017-09-06 23:28:06 -0300

    Testing the BST API and fixing bugs on the deleteMin & deleteMax methods

--- trees/binary-search-tree.js
+++ trees/binary-search-tree.js
@@ -1,9 +1,26 @@
 "use strict";
 
+function computeNodeSize(node) {
+  return (
+    1 +
+    ((node.right && node.right.size) || 0) +
+    ((node.left && node.left.size) || 0)
+  );
+}
+
+function BSTreeNode(value) {
+  return {
+    value: value,
+    left: null,
+    right: null,
+    size: 1
+  };
+}
+
 const ValueNotFoundAPI = {
-  parentTree: null,
-  setParentTree: function(tree) {
-    this.parentTree = tree;
+  parentNode: null,
+  setParentNode: function(node) {
+    this.parentNode = node;
     return this;
   }
 };
@@ -16,34 +33,53 @@ function isValueNotFound(x) {
   return ValueNotFoundAPI.isPrototypeOf(x);
 }
 
-const BTreeAPI = {
+const BSTreeAPI = {
+  size: 0,
+  root: null,
   insert: insert,
-  contains: contains
+  contains: contains,
+  deleteMax: deleteMax,
+  deleteMin: deleteMin,
+  preOrderTraversal: preOrderTraversal,
+  inOrderTraversal: inOrderTraversal,
+  postOrderTraversal: postOrderTraversal
 };
 
-function insert(value) {
-  var result = searchNode(this, value);
-  if (isValueNotFound(result)) {
-    let parentTree = result.parentTree;
-    if (parentTree.value > value) {
-      parentTree.left = BTree(value);
-    } else {
-      parentTree.right = BTree(value);
-    }
-  } else {
-    throw new Error("You cannot repeat values.");
+function _insert(node, value) {
+  if (node.value === value) {
+    throw new Error("The element was already present in the tree.");
+  } else if (node.value > value && node.left) {
+    node.left = _insert(node.left, value);
+  } else if (node.value < value && node.right) {
+    node.right = _insert(node.right, value);
+  } else if (node.value > value && !node.left) {
+    node.left = BSTreeNode(value);
+  } else if (node.value < value && !node.right) {
+    node.right = BSTreeNode(value);
   }
+  node.size = computeNodeSize(node);
+  return node;
 }
 
-function searchNode(tree, value) {
-  if (tree.value === value) return tree;
-  else if (tree.value > value && tree.left) {
-    return searchNode(tree.left, value);
-  } else if (tree.value < value && tree.right) {
-    return searchNode(tree.right, value);
+function insert(value) {
+  if (!this.root) {
+    this.root = BSTreeNode(value);
   } else {
-    return ValueNotFound().setParentTree(tree);
+    this.root = _insert(this.root, value);
   }
+  this.size = this.root.size;
+  return this.root;
+}
+
+function searchNode(node, value) {
+  if (node.value === value) return node;
+  if (node.value > value && node.left) {
+    return searchNode(node.left, value);
+  }
+  if (node.value < value && node.right) {
+    return searchNode(node.right, value);
+  }
+  return ValueNotFound().setParentNode(node);
 }
 
 function contains(value) {
@@ -51,12 +87,81 @@ function contains(value) {
   return !isValueNotFound(result);
 }
 
-function BTree(value) {
-  var newBTree = Object.create(BTreeAPI);
-  newBTree.value = value;
-  newBTree.left = null;
-  newBTree.right = null;
+function _deleteMax(node) {
+  if (node.right) {
+    node.right = _deleteMax(node.right);
+    node.size = node.right && node.right.size + node.left && node.left.size;
+    return node;
+  }
+  return node.left ? node.left : null;
+}
+
+function deleteMax() {
+  if (this.size > 0) {
+    this.root = _deleteMax(this.root);
+    this.size--;
+  }
+}
+
+function _deleteMin(node) {
+  if (node.left) {
+    node.left = _deleteMin(node.left);
+    node.size =
+      1 +
+      ((node.right && node.right.size) || 0) +
+      ((node.left && node.left.size) || 0);
+    return node;
+  }
+  return node.right ? node.right : null;
+}
+
+function deleteMin() {
+  if (this.size > 0) {
+    this.root = _deleteMin(this.root);
+    this.size--;
+  }
+}
+
+function _preOrderTraversal(node, cb) {
+  if (!node) return;
+  cb(node.value);
+  _preOrderTraversal(node.left, cb);
+  _preOrderTraversal(node.right, cb);
+}
+
+function preOrderTraversal(cb) {
+  _preOrderTraversal(this.root, cb);
+}
+
+function _inOrderTraversal(node, cb) {
+  if (!node) return;
+  _inOrderTraversal(node.left, cb);
+  cb(node.value);
+  _inOrderTraversal(node.right, cb);
+}
+
+function inOrderTraversal(cb) {
+  _inOrderTraversal(this.root, cb);
+}
+
+function _postOrderTraversal(node, cb) {
+  if (!node) return;
+  _postOrderTraversal(node.left, cb);
+  _postOrderTraversal(node.right, cb);
+  cb(node.value);
+}
+
+function postOrderTraversal(cb) {
+  _postOrderTraversal(this.root, cb);
+}
+
+function BSTree(value) {
+  var newBTree = Object.create(BSTreeAPI);
+  if (value) {
+    newBTree.root = BSTreeNode(value);
+    newBTree.size = 1;
+  }
   return newBTree;
 }
 
-module.exports = { BTree };
+module.exports = { BSTree };
--- trees/binary-search-tree.test.js
+++ trees/binary-search-tree.test.js
@@ -1,12 +1,83 @@
-const { BTree } = require("./binary-search-tree");
+const { BSTree } = require("./binary-search-tree");
 
-var values = [200, 50, 70, 76, 26, 65, 150];
-var tree = BTree(100);
-
-values.forEach(v => {
-  tree.insert(v);
+describe("Creation", () => {
+  test("The size of a empty BST should be 0", () => {
+    expect(BSTree().size).toBe(0);
+  });
+  test("The size of an initialized BST should be 1", () => {
+    expect(BSTree(100).size).toBe(1);
+  });
 });
 
-console.log(JSON.stringify(tree, null, 2));
+describe("Insertion", () => {
+  var tree = BSTree();
+  tree.insert(20);
+  tree.insert(30);
+  tree.insert(10);
+  tree.insert(12);
+  tree.insert(5);
+  test("The tree should have the correct size", () => {
+    expect(tree.size).toBe(5);
+  });
+});
 
-console.log(values.length === values.filter(v => tree.contains(v)).length);
+describe("Deletion", () => {
+  /**
+   *         20
+   *     10      30
+   *    5  15  25  35
+   */
+  var tree = BSTree();
+  var values = [20, 10, 30, 15, 5, 25, 35];
+  values.forEach(v => {
+    tree.insert(v);
+  });
+  test("Deleting the min element should reduce the size of the tree by one", () => {
+    tree.deleteMin();
+    /**
+    *         20
+    *     10      30
+    *    X  15  25  35
+    */
+    expect(tree.size).toBe(6);
+  });
+  test("The min value shouldn't be present in the tree", () => {
+    var remainingValues = [];
+    tree.inOrderTraversal(v => remainingValues.push(v));
+    expect(remainingValues.indexOf(5)).toBe(-1);
+  });
+  test("Deleting the max element should reduce the size of the tree by one", () => {
+    tree.deleteMax();
+    /**
+    *         20
+    *     10      30
+    *    X  15  25  X
+    */
+    expect(tree.size).toBe(5);
+  });
+  test("The max value shouldn't be present in the tree", () => {
+    var remainingValues = [];
+    tree.inOrderTraversal(v => remainingValues.push(v));
+    expect(remainingValues.indexOf(35)).toBe(-1);
+  });
+  test("Deleting the root should promote a new root", () => {
+    tree.deleteMax();
+    tree.deleteMax();
+    tree.deleteMax();
+    /**
+    *         10
+    *      15    X 
+    */
+    var remainingValues = [];
+    tree.inOrderTraversal(v => remainingValues.push(v));
+    expect(tree.root.value).toBe(10);
+    expect(tree.size).toBe(2);
+  });
+  test("Deleting all nodes should make the root equal to null", () => {
+    while (tree.size > 0) {
+      tree.deleteMin();
+    }
+    expect(tree.size).toBe(0);
+    expect(tree.root).toBeNull();
+  });
+});
