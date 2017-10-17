e8a73820aa2860073a1f36feaaee67c769ea2b10
Renzo Canepa <rcanepag@gmail.com>
2017-10-16 22:46:02 -0300

    Initial commit for the MSD string sorting algorithm in Python

---
+++ python/strings/msd_string_sort.py
@@ -0,0 +1,14 @@
+"""MSD (most significant digit) string sort algorithm.
+
+"""
+
+
+def msd_sort(strings):
+    if not isinstance(strings, list):
+        raise Exception("A {} was provided instead of a list of strings.".format(type(strings)))
+
+    return "=)"
+
+
+if __name__ == "__main__":
+    print(msd_sort([]))
