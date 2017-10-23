8a2fb10baadaa4ae330cd4d7b5bcdf0937a5bf29
Renzo Canepa <rcanepag@gmail.com>
2017-10-22 21:33:05 -0300

    Implementation of the solution to longest repeated substring problem in Python (using a Suffix Array)

---
+++ python/interview_questions/longest_repeated_substring.py
@@ -0,0 +1,49 @@
+"""Longest repeated substring (or LCP = longest common prefix in a suffix array).
+Problem: find the longest repeated substring inside a string.
+
+Steps:
+    1. Create suffixes. This should be linear in time and space, but it isn't.
+        Slicing strings in Python (with slice or [a:b]) is a linear operation
+        with regard to the size of the string. In the end, this implementation
+        provides a quadratic time O(N^2).
+    2. Sort suffixes. This should be N * log(N) in time.
+    3. Find LCP between adjacent suffixes.
+"""
+
+
+def lcp(s1, s2):
+    """Return the length of the longest common prefix
+    between strings `s1` and `s2`."""
+    comp = 0
+    for i in range(min(len(s1), len(s2))):
+        if s1[i] != s2[i]:
+            break
+        comp += 1
+    return comp
+
+
+def lrs(text):
+    """Return the longest repeated substring using a Suffix Array."""
+    # Step 1: create the suffixes array.
+    suffixes = []
+    for i in range(len(s)):
+        suffixes.append(s[i:])
+
+    # Step 2: sort the suffixes array.
+    sorted_suffixes = sorted(suffixes)
+
+    # Step: find the longest repeated substring.
+    result = ""
+    for i in range(len(sorted_suffixes) - 1):
+        l = lcp(sorted_suffixes[i], sorted_suffixes[i + 1])
+        if l > len(result):
+            result = sorted_suffixes[i][:l]
+
+    return result
+
+
+if __name__ == "__main__":
+    s = "mississipi"
+    r = lrs(s)
+    print(r)
+    assert r == "issi"
