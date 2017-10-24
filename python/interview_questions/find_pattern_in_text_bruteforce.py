bbce978eb64cb472584302567727b35eb18e10df
Renzo Canepa <rcanepag@gmail.com>
2017-10-24 16:35:31 -0300

    Adding docstrings to functions and improving readability

--- python/interview_questions/find_pattern_in_text_bruteforce.py
+++ python/interview_questions/find_pattern_in_text_bruteforce.py
@@ -1,24 +1,30 @@
 """Return the first occurrence of a pattern in a string. Removing
 the break statements will make both versions return all occurrences.
-This two implementations are slow ~ O(N * M). N = length of the
-string and M = length of the pattern."""
+If N is the length of the string and M the length of the pattern,
+brute force implementation run in O(N * M) time in the worst case.
+
+The worst case is one in which the pattern resembles very closely
+the string in multiple parts. For example:
+
+- Pattern: AAAC
+- String: AAAAAAAAAAAAAAAAC
+
+In this case, every character of the string has to be compared to
+all the characters of the pattern. The mismatch is always at the
+end of the comparison.
+
+However, in some cases, this algorithm could perform close to
+O(N + M).
+"""
 
 
 def find_pattern_v1(s, p):
     """Return the first position of `p` in `s` or the length
-    of `s` if no match is found."""
+    of `s` if no match is found. In this case `i` doesn't move
+    forward until there is a mismatch between the pattern and
+    the part of the string being examined. `j` moves back and
+    forth for every value of `i`."""
     ls, lp = len(s), len(p)
-    for i in range(ls - lp + 1):
-        if p == s[i:i + lp]:
-            return i
-    return ls
-
-
-def find_pattern_v2(s, p):
-    """Return the first position of `p` in `s` or the length
-    of `s` if no match is found."""
-    ls, lp = len(s), len(p)
-    j = 0
     for i in range(ls):
         for j in range(lp):
             if s[i + j] != p[j]:
@@ -28,15 +34,42 @@ def find_pattern_v2(s, p):
     return ls
 
 
-if __name__ == "__main__":
-    text = "abracadabra"
-    pattern = "bra"
-    p1 = find_pattern_v1(text, pattern)
-    print("-> Result v1:")
-    print(text)
-    print(" " * p1 + pattern)
-    p2 = find_pattern_v2(text, pattern)
-    print("-> Result v2:")
-    print(text)
-    print(" " * p2 + pattern)
+def find_pattern_v2(s, p):
+    """Return the first position of `p` in `s` or the length
+    of `s` if no match is found. In this case `i` goes back and
+    forth along with `j`. When there is a mismatch, `i` goes back
+    `j` positions to the start of a new search."""
+    ls, lp = len(s), len(p)
+    i = j = 0
+    while i < ls:
+        if s[i] == p[j]:
+            j += 1
+        else:
+            i -= j  # Move back `i` `j` positions.
+            j = 0
+        i += 1  # Move `i` forward.
 
+        # The pattern was found.
+        if j == lp:
+            return i - lp
+    return ls
+
+
+if __name__ == "__main__":
+    test_cases = [
+        ("abracadabra", "bra"),
+        ("aaaaaaab", "ab"),
+        ("cxccc", "c"),
+        ("this pattern is not a common pattern!", "!")
+    ]
+    for test_case, (text, pattern) in enumerate(test_cases):
+        print("############# Test case {} #############".format(test_case))
+        p1 = find_pattern_v1(text, pattern)
+        print("-> Result v1:")
+        print(text)
+        print(" " * p1 + pattern)
+        p2 = find_pattern_v2(text, pattern)
+        print("-> Result v2:")
+        print(text)
+        print(" " * p2 + pattern)
+        print("#######################################")
